3da12eda7b125e440b8d5a854f6918e1dac9e868
SuosSovanrith <suos089@gmail.com>
2023-04-17 18:01:21 +0700

    Last Commit

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
 
-        // var t = ;
-        document.getElementById('ddmmyy').value = new_Date().toISOString().substring(0, 10);
-        function date_new() {
-            var time5 = (document.getElementById("ddmmyy").valueAsNumber);
-            var getmonth = (document.getElementById("ddmmyy").valueAsDate.getMonth());
-            var getday = (document.getElementById("ddmmyy").valueAsDate.getDate());
-            var getMonthsubstract = (getmonth ) * 30;
-            var getdaysubstract = (getday );
-            var t = Math.abs(getMonthsubstract + getdaysubstract);
-            console.log(cal);
-            console.log(getmonth);
-            console.log(getday);
-            console.log(t);
-            var A = 357;
-            var incos = (((2 * (3.14)) / 365) * (t - 172));
-            var cos = Math.cos(incos);
-            var C = 739.5;
-            var result = ((A * cos) + C) * 10;
-            var xy = Math.floor(result);
-            xy /= 10;
-            var yx = Math.floor(xy / 60);
-            document.getElementsByClassName("result1").innerHTML = xy;
-            document.getElementById("alaskaHour").innerHTML = yx;
-        }
+function Cal1() 
+{
+    var getMonth = (document.getElementById("ddmmyy1").valueAsDate.getMonth());
+    var getMonthNum = (getMonth ) * 30;
+    var getDay = (document.getElementById("ddmmyy1").valueAsDate.getDate());
+    var t = Math.abs(getMonthNum + getDay);
+    var C = 727.608;
+    var A = 43.725;
+    var incos = (((2 * (3.14)) / 365) * (t - 172));
+    var cos = Math.cos(incos);
+    var result = ((A * cos) + C) * 10;
+    result /= 10;
+    var resultFinal = result.toFixed(3);
+    document.getElementById("result1").innerHTML = resultFinal;
+}    
+       
+function Cal2() 
+{
+    var getMonth = (document.getElementById("ddmmyy2").valueAsDate.getMonth());
+    var getMonthNum = (getMonth ) * 30;
+    var getDay = (document.getElementById("ddmmyy2").valueAsDate.getDate());
+    var t = Math.abs(getMonthNum + getDay);    
+    var C = 727.558;
+    var A = 37.225;
+    var incos = (((2 * (3.14)) / 365) * (t - 172));
+    var cos = Math.cos(incos);
+    var result = ((A * cos) + C) * 10;
+    result /= 10;
+    var resultFinal = result.toFixed(3);
+    document.getElementById("result2").innerHTML = resultFinal;
+}    
     
-        var time5 = (document.getElementById("ddmmyy").valueAsNumber);
-        var getmonth = (document.getElementById("ddmmyy").valueAsDate.getMonth() + 1);
-        var getday = (document.getElementById("ddmmyy").valueAsDate.getDate());
-        var cal = (((((time5 / 1000) / 60) / 60) / 24) / 30) / 12;
-        var getMonthsubstract = (getmonth - 6) * 30;
-        var getdaysubstract = (getday - 21) - 2;
-        var t = Math.abs(getMonthsubstract + getdaysubstract);
-        while (t >= 173) {
-            t -= 1;
-        }
-        var A = 357;
-        var incos = (((2 * (3.14)) / 365) * (t - 172));
-        var cos = Math.cos(incos);
-        var C = 739.5;
-        var result = ((A * cos) + C) * 10;
-        var xy = (Math.floor(result));
-        xy /= 10;
-        var yx = Math.floor(xy / 60);
-        document.getElementsByClassName("result1").innerHTML = xy;
-        document.getElementById("alaskaHour").innerHTML = yx;
     
